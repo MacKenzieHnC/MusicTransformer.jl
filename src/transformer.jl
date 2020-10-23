2e3225b16f8e62744f783130c4b446df9ac86cf1
MacKenzieHnC <MacKenzieHnC@gmail.com>
2020-10-22 18:27:11 -0700

    Added clarity

--- src/transformer.jl
+++ src/transformer.jl
@@ -1,7 +1,35 @@
 using Flux
 using TensorCast
+using CUDA: CuArray
 
 ## Attention
+
+#=
+    The attention module is made of 5 dense layers.
+    The first 4 linearly project the input data and position data
+    so they can be processed by later parts of the module.
+
+    The last dense layer combines the attention calculations from the different
+    heads into the module's final output
+
+    Position data is currently assumed to be relative. It accepts a tensor
+    of size (word_size, 2 * num_timesteps - 1, 2 * num_instruments - 1)
+
+    I'm struggling with a good explanation for this, but basically the
+    pos_data is in the form [..., -3, -2, -1, 0, 1, 2, 3, ...] for however
+    long the sequence is. Then, when we're looking at the (i,j)th query,
+    we only consider the range in the position data that applies.
+    e.g. if we're looking at the last note in the song, there is no
+    timestep that is 1 more than that, so we only consider the positions
+    from [-n, 0].
+
+    And instead of just using the distances, we use some embedding
+    with dimension (word_size) for each possible distance.
+
+    I use the embedding from the original paper, but this file is
+    indifferent to the embedding, as long as it meets the other requirements.
+=#
+
 struct Attention{
     S<:AbstractArray,
     T<:AbstractArray,
@@ -9,11 +37,11 @@ struct Attention{
     V<:AbstractArray,
     W<:AbstractArray,
 }
-    W_Q::S
-    W_K::T
-    W_V::U
-    W_P::V
-    W_O::W
+    W_Q::S # Dense layer for queries
+    W_K::T # Dense layer for keys
+    W_V::U # Dense layer for values
+    W_P::V # Dense layer for positions
+    W_H::W # Dense layer for heads
 end
 
 function Attention(
@@ -38,36 +66,44 @@ function (a::Attention)(
     pos_data::AbstractArray;
     key_data = nothing::Union{Nothing,AbstractArray}
 )
-    W_Q, W_K, W_V, W_P, W_O = a.W_Q, a.W_K, a.W_V, a.W_P, a.W_O
+    W_Q, W_K, W_V, W_P, W_H = a.W_Q, a.W_K, a.W_V, a.W_P, a.W_H
 
-    # Self-attention
+    # For self-attention, keys and queries come from the same input
     if key_data == nothing
         key_data = query_data
     end
 
+    # Perform the first 4 linear projections
     @reduce my_values[head,meaning,time,ins] := sum(word) W_V[head,meaning,word] * key_data[word,time,ins]
     @reduce my_queries[head,meaning,time,ins] := sum(word) W_Q[head,meaning,word] * query_data[word,time,ins]
     @reduce my_keys[head,meaning,time,ins] := sum(word) W_K[head,meaning,word] * key_data[word,time,ins]
     @reduce my_pos[head,meaning,time,ins] := sum(word) W_P[head,meaning,word] * pos_data[word,time,ins]
 
-    # Get scores
+    # For every word in the query projection (i, j)
+    # Add the key projection to the relevant section of the position projection
+    # Then multiply by the (i, j)th query projection
     scores = []
     for i in 1:size(my_queries)[3]
         ins_scores = []
         for j in 1:size(my_queries)[4]
             Q = my_queries[:,:,i,j]
-            E = my_keys .+ my_pos[:, :, i:(i+size(my_queries)[3]-1),
-                                        j:(j+size(my_queries)[4]-1)]
+            P = my_pos[:, :, i:(i+size(my_queries)[3]-1),
+                                j:(j+size(my_queries)[4]-1)]
+            K = my_keys
+
+            E = P .+ K
             @reduce C[head,q_time,q_ins,k_time,k_ins] := sum(meaning) Q[head,meaning,q_time,q_ins] * E[head,meaning,k_time,k_ins]
 
-            # Bounce up
+            # Pass the information up from the for-loop
+            #   in a way that doesn't break Flux.gradient()
+            # I'm sure there's a better way, but I don't know it
             if ins_scores == []
                 ins_scores = C
             else
                 ins_scores = cat(ins_scores, C, dims = 3)
             end
         end
-        # Bounce up again
+        # Pass the information up again
         if scores == []
             scores = ins_scores
         else
@@ -110,11 +146,14 @@ function (a::Attention)(
 
     # Transform back to input dimensions
     @reduce z[word, time, ins] :=
-        sum(long_meaning) W_O[word, long_meaning] *
+        sum(long_meaning) W_H[word, long_meaning] *
         scaled_values[long_meaning, time, ins]
 end
 
 ## Encoder
+
+# An encoder is just a LayerNorm followed by an attention layer,
+# With a residual connection and a relu on the end
 struct Encoder
     norm::LayerNorm
     self_attn::Attention
@@ -135,6 +174,9 @@ function (a::Encoder)(query_data::AbstractArray, pos_data::AbstractArray)
 end
 
 ## Decoder
+# A decoder is just two encoders stacked on top of each other, with the
+# second one getting it's key and value data from somewhere else.
+
 struct Decoder
     norm1::LayerNorm
     self_attn::Attention
@@ -165,7 +207,9 @@ function (a::Decoder)(query_data::AbstractArray,
 end
 
 ## Transformer
-
+# The transformer just brings some number of encoders and decoders together,
+# with the cross-attention in the decoders getting their key and value data
+# from the final output of the encoder chain.
 struct Transformer
     encoder::Union{Encoder,Chain}
     decoder::Union{Decoder,Chain}
@@ -185,11 +229,13 @@ end
 
 Flux.@functor Transformer
 
-function (a::Transformer)(decoder_input, pos_data; encoder_input = nothing)
+function (a::Transformer)(encoder_input,
+                            pos_data;
+                            decoder_input = nothing)
     encoder, decoder = a.encoder, a.decoder
 
-    if encoder_input == nothing
-        encoder_input = decoder_input
+    if decoder_input = nothing
+        decoder_input = encoder_input
     end
 
     encoded_data = encoder_input
@@ -201,9 +247,6 @@ function (a::Transformer)(decoder_input, pos_data; encoder_input = nothing)
     for d in decoder
         output = d(output, encoded_data, pos_data)
     end
-
-    # If using onehot
-    #return Flux.softmax(output, dims=1)
 
     return output
 end
