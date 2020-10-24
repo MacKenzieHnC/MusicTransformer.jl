730c830a18de864ee1074a396668045f83dc37d7
MacKenzieHnC <MacKenzieHnC@gmail.com>
2020-10-23 23:10:48 -0500

    Remove instrument from positional encoding
    There's a better way to do that, but idk what it is yet

--- src/positional_encoding.jl
+++ src/positional_encoding.jl
@@ -5,7 +5,7 @@ function get_positional_encoding(song::AbstractArray)
     sequence_length = size(song)[2]
     num_ins = size(song)[3]
 
-    pos_data = [k%2==0 ? sin(1/(10000^(2*k/word_size))*t*i) : cos(1/(10000^(2*k/word_size))*t*i)
+    pos_data = [k%2==0 ? sin(1/(10000^(2*k/word_size))*t) : cos(1/(10000^(2*k/word_size))*t)
                     for k in 1:word_size,
                         t in (-sequence_length+1):(sequence_length-1),
                         i in (-num_ins+1):(num_ins-1)]
