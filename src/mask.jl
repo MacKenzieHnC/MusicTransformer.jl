3de8f59433c31ff26d6ded6f8ec3da1810cd033b
MacKenzieHnC <MacKenzieHnC@gmail.com>
2020-10-27 11:32:58 -0500

    Just renamed function

--- src/mask.jl
+++ src/mask.jl
@@ -1,6 +1,6 @@
 using Random
 
-function mask(region::AbstractArray, scramble_rate)
+function get_mask(region::AbstractArray, scramble_rate)
     mask = zeros(1, 1, 1, size(region)[2:end]...)
     num_masked = Integer(floor(scramble_rate * size(region)[2] * size(region)[3]))
     idx = [(i,j) for i in 1:size(region)[2], j in 1:size(region)[3]]
